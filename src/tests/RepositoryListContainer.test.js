ff110ce78d7df63492d4cb4f6ce166d427f4c7e4
Stefano Cicatiello <stefano.cicatiello@gmail.com>
2018-12-16 21:53:09 +0100

    tests

---
+++ src/tests/RepositoryListContainer.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MockedProvider } from 'react-apollo/test-utils';
+
+import RepositoryListContainer from '../containers/RepositoryListContainer';
+import GET_REPOSITORIES from '../graphql/queries/getRepositories';
+import GET_SELECTED_REPOSITORIES from '../graphql/queries/getSelectedRepositories';
+
+
+const wait = require('waait');
+
+const mocks = [
+    {
+        request: {
+                query: GET_REPOSITORIES,
+        },
+        result: {
+            data: {
+                repositories: [
+                    { id: 1, name: 'Repo1' },
+                    { id: 2, name: 'Repo2' },
+                ],
+            },
+        },
+    },
+    {
+        request: {
+                query: GET_SELECTED_REPOSITORIES,
+        },
+        result: {
+            data: {
+                selectedRepositories: [1]
+            },
+        },
+    },
+];
+
+let component = null;
+
+xdescribe('Repository List Container component', () => {
+
+    beforeEach(() => {
+        component = renderer.create(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <RepositoryListContainer />
+            </MockedProvider>,
+        );
+    });
+
+    it('renders table contents', async () => {
+        await wait(0); // wait for response
+
+        console.log(component.toJSON())
+        const table = component.root.findByType('table');
+        expect(table.children).toContain('Repo1');
+    });
+});
---
+++ src/tests/SelectRepository.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MockedProvider } from 'react-apollo/test-utils';
+
+import SelectRepository from '../containers/SelectRepository';
+import SELECT_REPOSITORY from '../graphql/mutations/selectRepository';
+
+const wait = require('waait');
+
+
+const selectRepository = { id: 1, isSelected: false };
+const mocks = [{
+    request: {
+        query: SELECT_REPOSITORY,
+        variables: { id: 1, isSelected: false },
+    },
+    result: { data: { selectRepository } },
+},];
+
+xdescribe('Select Repository component', () => {
+
+    it('renders', async () => {
+        const component = renderer.create(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <SelectRepository />
+            </MockedProvider>,
+        );
+
+        const button = component.root.findByType('button');
+        expect(button.children).toContain('Select');
+        button.props.onClick(); // fires the mutation
+
+        await wait(0);
+
+        //expect(button.children).toContain('Unselect');
+    })
+});
